4cf9008faa35c3e8d5ccf62626fe8329d6e408d7
Jia <jiakhot@outlook.com>
2024-11-29 15:37:26 +0530

    ui stuff

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.plaf.basic.BasicScrollBarUI;
 
 import java.awt.*;
 import java.io.File;
@@ -12,12 +13,17 @@ public class App {
     private final static Color[] COLORS = {
         new Color(0x121213), //0 grey
         new Color(0xD924A5), //1 pink
-        new Color(0x0ECAD1)  //2 blue
+        new Color(0x0ECAD1),  //2 blue
+        new Color(0x3A3A3A)  //3 light grey
     };
 
     private final static Font defaultFont = new Font("OCR A Extended", Font.PLAIN, 16);
     private final static Font menuFont = new Font("OCR A Extended", Font.PLAIN, 16);
 
+    private static JFileChooser saveFileChooser = new JFileChooser();
+    private static JFileChooser openFileChooser = new JFileChooser();
+    private static JMenu fileMenu = createFileMenu();
+    private static JMenuBar menuBar = createMenuBar();
     private static JFrame frame = createFrame();
     private static JEditorPane editorPane = createEditorPane(frame);
 
@@ -41,8 +47,6 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1200, 600);
         frame.setUndecorated(true);
-
-        JMenuBar menuBar = createMenuBar();
         frame.setJMenuBar(menuBar);
 
         JRootPane rootPane = frame.getRootPane();
@@ -64,6 +68,53 @@ public class App {
         editorPane.setMargin(new Insets(20, 20, 20, 20));
 
         JScrollPane scrollPane = new JScrollPane(editorPane);
+
+        JScrollBar scrollBar = scrollPane.getVerticalScrollBar();
+
+        scrollBar.setUI(new BasicScrollBarUI() {
+
+            @Override 
+            public void configureScrollBarColors() {
+
+                this.thumbColor = COLORS[2];
+                this.trackColor = COLORS[0];
+                
+            }
+
+            @Override
+            public Dimension getPreferredSize(JComponent C) {
+
+                return new Dimension(10, super.getPreferredSize(C).height);
+
+            }
+
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+
+                return formattedButton();
+
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+
+                return formattedButton();
+
+            }
+
+            private JButton formattedButton() {
+
+                JButton b = new JButton();
+                
+                Dimension dim = new Dimension(0, 0);
+                b.setPreferredSize(dim);
+                b.setMaximumSize(dim);
+
+                return b;
+            }
+
+        });
+
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
@@ -89,12 +140,12 @@ public class App {
 
     }
 
-    private static JMenuBar createMenuBar() {
+    private static JMenu createFileMenu() {
 
-        JMenuBar menuBar = new JMenuBar();
-        menuBar.setBackground(COLORS[0]);
-        menuBar.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, COLORS[2]));
-        menuBar.setPreferredSize(new Dimension(600, 40));
+        UIManager.put("Menu.selectionBackground", COLORS[3]);
+        UIManager.put("Menu.selectionForeground", COLORS[2]);
+        UIManager.put("MenuItem.selectionBackground", COLORS[3]);
+        UIManager.put("MenuItem.selectionForeground", COLORS[2]);
 
         JMenu fileMenu = new JMenu("File");
         fileMenu.setFont(menuFont);
@@ -115,12 +166,10 @@ public class App {
 
         openItem.addActionListener(l -> {
 
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Text Documents (*.txt)", "txt"));
-            int option = fileChooser.showOpenDialog(frame);
+            int option = openFileChooser.showOpenDialog(frame);
 
             if (option == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
+                File file = openFileChooser.getSelectedFile();
                 try {
                     String fileContent = Files.readString(file.toPath());
                     editorPane.setText(fileContent);
@@ -135,18 +184,18 @@ public class App {
 
         saveItem.addActionListener(l -> {
         
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new FileNameExtensionFilter("Text Documents (*.txt)", "txt"));
-            int option = fileChooser.showSaveDialog(frame);
+            saveFileChooser.setFileFilter(new FileNameExtensionFilter("Text Documents (*.txt)", "txt"));
+            int option = saveFileChooser.showSaveDialog(frame);
 
             if (option == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
+                File file = saveFileChooser.getSelectedFile();
                 FileWriter fileWriter = null;
 
                 try {
                     fileWriter = new FileWriter(file);
                     fileWriter.write(editorPane.getText());
                     JOptionPane.showMessageDialog(null, "File saved.");
+                    saveFileChooser.rescanCurrentDirectory();
                 } catch(Exception e) {
                     JOptionPane.showMessageDialog(null, "Error saving file.");
                 } finally {
@@ -172,6 +221,17 @@ public class App {
         fileMenu.add(newItem);
         fileMenu.add(saveItem);
         fileMenu.add(exitItem);
+
+        return fileMenu;
+
+    }
+
+    private static JMenuBar createMenuBar() {
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBackground(COLORS[0]);
+        menuBar.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 0, COLORS[2]));
+        menuBar.setPreferredSize(new Dimension(600, 40));
 
         menuBar.add(fileMenu);
 
